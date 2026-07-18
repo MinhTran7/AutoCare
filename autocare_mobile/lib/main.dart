e28d50cdfae37179f0f992d5b7265887f7bbc2e6
NguyenTrungHieu16 <nzbhsg@gmail.com>
2026-07-18 21:24:41 +0700

    sua lai main dart

--- autocare_mobile/lib/main.dart
+++ autocare_mobile/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting('vi_VN', null);
+
   runApp(const AutoCareApp());
 }
